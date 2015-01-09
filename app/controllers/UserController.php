4f1351dca270f8724778e6c436b6f7c8c0f814fb
Hitesh Mittal <hiteshmittal1990@gmail.com>
2015-01-09 21:48:07 +0530

    user login created

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -19,26 +19,26 @@ class UserController extends BaseController
 
     public function postLogin()
     {
-//        try {
-//            $this->rules->validate($input = Input::only('email', 'password'));
-//            Sentry::authenticate($input, Input::has('remember'));
-//        } catch (\Cartalyst\Sentry\Users\UserNotFoundException $e) {
-//            return Redirect::back()->withInput()->withErrorMessage('Invalid credentials provided');
-//        } catch (\Cartalyst\Sentry\Users\UserNotActivatedException $e) {
-//            return Redirect::back()->withInput()->withErrorMessage('User Not Activated.');
-//        } catch (\Laracasts\Validation\FormValidationException $e) {
-//            return Redirect::back()->withInput()->withErrors($e->getErrors());
-//        }
-//        // Logged in successfully - redirect based on type of user
-//        $user = Sentry::getUser();
-//        $admin = Sentry::findGroupByName('admins');
-//        $users = Sentry::findGroupByName('users');
-//        if ($user->inGroup($admin))
-//            return Redirect::intended('admin');
-//        elseif ($user->inGroup($users))
-//            return Redirect::intended('user');
-//        else
-//            return Redirect::intended('/');
+       try {
+           $input = Input::only('email', 'password');
+           Sentry::authenticate($input, Input::has('remember'));
+       } catch (\Cartalyst\Sentry\Users\UserNotFoundException $e) {
+           return Redirect::back()->withInput()->withErrorMessage('Invalid credentials provided');
+       } catch (\Cartalyst\Sentry\Users\UserNotActivatedException $e) {
+           return Redirect::back()->withInput()->withErrorMessage('User Not Activated.');
+       } catch (\Laracasts\Validation\FormValidationException $e) {
+           return Redirect::back()->withInput()->withErrors($e->getErrors());
+       }
+       // Logged in successfully - redirect based on type of user
+       $user = Sentry::getUser();
+       $admin = Sentry::findGroupByName('admin');
+       $users = Sentry::findGroupByName('users');
+       if ($user->inGroup($admin))
+           return Redirect::intended('/');
+       elseif ($user->inGroup($users))
+           return Redirect::intended('user');
+       else
+           return Redirect::intended('/');
     }
 
 }
--- app/views/site/user/login.blade.php
+++ app/views/site/user/login.blade.php
@@ -6,7 +6,34 @@
 
 {{-- Content --}}
 @section('content')
-    
+<div class="content-inner">
+
+    {{ Form::open(array('url' => 'user/login')) }}
+    <h1 id="page-title" class="page__title title">User account</h1>
+    @if (Session::has('flash_message'))
+    <p style="padding:5px" class="bg-success text-success">{{ Session::get('flash_message') }}</p>
+    @endif
+    @if (Session::has('error_message'))
+    <p style="padding:5px" class="bg-danger text-danger">{{ Session::get('error_message') }}</p>
+    @endif
+    <div class="form-item form-type-textfield form-item-name">
+        {{ Form::label('email', 'Email') }}
+        {{ Form::text('email', null, ['class' => 'form-control', 'required' => 'required'])}}
+        <div class="description">Enter your webserver12.com username.</div>
+    </div>
+    <div class="form-item form-type-password form-item-pass">
+        {{ Form::label('password', 'Password') }}
+        {{ Form::password('password', array( 'class' => 'form-control', 'required' => 'required'))}}
+        <div class="description">Enter the password that accompanies your username.</div>
+    </div>
+
+    <div class="login-footer">                                                               
+        {{ Form::submit('Log In', ['class' => 'btn btn-lg btn-primary btn-block']) }}            
+    </div>
+    {{ Form::close() }}
+</div>
+<div style="clear: both;"></div>
+
 @stop
 
 
